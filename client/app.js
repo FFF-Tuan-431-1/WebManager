727759633ecaef40403bd6655575cc163424cba7
lyk <1756633219@qq.com>
2016-01-05 17:20:07 +0800

    dev-socket

--- client/app.js
+++ client/app.js
@@ -12,4 +12,5 @@ socket.on('connect', function(){
   });
 });
 
-socket.on('disconnect', function(){});
+socket.on('disconnect', function(){
+});
--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,4 @@
 var Client = require('../models/client').Client;
-var State = require('../models/client').State;
 
 var randomString = function() {
   return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
@@ -12,16 +11,34 @@ module.exports = function(socket) {
     console.log('remote client ip is ' + data.ip);
     console.log('remote client mac is ' + data.mac);
 
+    socket.data = data;
+
     Client.findOne({where: {mac: data.mac}}).then((client) => {
       if (!client) {
-        return Client.create({mac: data.mac, name: randomString()});
+        return Client.create({mac: data.mac, name: randomString(), isOnline:true});
       }
+      client.isOnline = true;
+      client.save();
       return Promise.resolve(client);
     }).then(function(client) {
      return client.createState({
-        ip: data.ip,
+        ip:data.ip,
         state: 'online'
       });
     });
+  })
+
+  socket.on('disconnect',function(){
+    console.log(socket.data.mac);
+    Client.findOne({where:{mac:socket.data.mac}}).then((client) => {
+      client.isOnline = false;
+      client.save();
+      return Promise.resolve(client);
+    }) .then(function(client){
+      return client.createState({
+        ip:socket.data.ip,
+        state:'offline'
+      })
+    })
   })
 };
