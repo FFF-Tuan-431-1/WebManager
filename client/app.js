da0decdc1cf13f206106a1e6cf78d2cdfa3e5e65
lcjnil <emlcjnil@gmail.com>
2016-01-05 14:49:23 +0800

    add mac info

--- client/app.js
+++ client/app.js
@@ -1,9 +1,15 @@
 var socket = require('socket.io-client')('http://localhost:3000');
 var ip = require('ip');
+var getmac = require('getmac');
 
 socket.on('connect', function(){
   console.log('connected to server');
-  socket.emit('ip', ip.address());
+  getmac.getMac((err, mac) => {
+    socket.emit('info', {
+      ip: ip.address(),
+      mac: mac
+    });
+  });
 });
 
 socket.on('disconnect', function(){});
--- socket/index.js
+++ socket/index.js
@@ -1,7 +1,8 @@
 module.exports = function(socket) {
   console.log('a new client is online');
 
-  socket.on('ip', function(ipAddr) {
-    console.log('remote client ip is ' + ipAddr);
+  socket.on('info', function(data) {
+    console.log('remote client ip is ' + data.ip);
+    console.log('remote client mac is ' + data.mac);
   })
 };
