a9d7c908acb5155d74c2c1c25c5074f34d5d6606
lcjnil <emlcjnil@gmail.com>
2016-01-05 19:44:38 +0800

    fix some syntax

--- routes/client.js
+++ routes/client.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var Client = require('../models/client').Client;
-var State = require('../models/client').State;
 var ping = require("net-ping");
 
 /*
  * 获取所有主机列表, 以 json 形式发送
  */
 router.get('/', (req, res) => {
-  Client.findAll().then((clients) => {
+  Client.findAll().then(clients => {
     if (!clients) {
       return res.status(400).json({error: 'clients is not exist'})
     }
-    res.json(clients);
+    res.status(200).json(clients);
   })
 });
 
@@ -20,7 +19,7 @@ router.get('/', (req, res) => {
  * 修改 id 为 req.param.id 的主机名字, 主机名字放在 req.body.name 中
  */
 router.put('/:id', (req, res) => {
-  Client.findOne({where: {id: req.params.id}}).then((client) => {
+  Client.findOne({where: {id: req.params.id}}).then(client => {
     if (!client) {
       return res.status(400).json({error: 'client is not exist'})
     }
@@ -33,16 +32,13 @@ router.put('/:id', (req, res) => {
  * 获取 主机的所有状态
  */
 router.get('/:id/state', (req, res) => {
-  Client.findOne({where: {id: req.params.id}}).then((client) => {
+  Client.findOne({where: {id: req.params.id}}).then(client => {
     if (!client) {
       return res.status(400).json({error: 'client is not exist'})
     }
     return client.getStates();
   }).then((states) => {
-    if (!states) {
-      return res.status(400).json({error: 'states is not exist'})
-    }
-    res.json(states);
+    res.status(200).json(states);
   })
 });
 
@@ -50,14 +46,14 @@ router.get('/:id/state', (req, res) => {
  * ping 主机, id 为 req.param.id
  */
 router.get('/:id/ping', (req, res) => {
-  Client.findOne({where: {id: req.params.id}}).then((client) => {
+  Client.findOne({where: {id: req.params.id}}).then(client => {
     if (!client) {
       return res.status(400).json({error: 'client is not exist'})
     }
     return client.getStates();
   }).then((states) => {
     if (!states) {
-      return res.status(400).json({error: 'states is not exist'});
+      return res.status(400).json({error: 'not knowing its ip'});
     }
     var ip = states.pop().ip;
     var session = ping.createSession();
@@ -66,7 +62,7 @@ router.get('/:id/ping', (req, res) => {
         return res.status(400).json({error: error});
       }
       res.status(200).json({time: r - s});
-    })
+    });
   })
 });
 
