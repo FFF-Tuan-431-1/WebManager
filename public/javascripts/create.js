58c72f95114385508605f0f60485d62e6f091c5d
lyk <1756633219@qq.com>
2016-01-10 16:01:24 +0800

    增加主机功能（增加正则表达式判断）

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
     var $btn = $('#btn-create');
     $btn.html($('<span>加载中 <i class="fa fa-spinner faa-spin animated"></i></span>'));
     $('.placeholder').text('').removeClass('success error');
+    var temp = /[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}/;
+    if (!temp.test($('#macCreate').val()))
+    {
+      $btn.text('添加主机');
+      $('.placeholder').text('请输入合法的mac地址!(mac地址格式为xx-xx-xx-xx-xx-xx) ').addClass('error');
+      return false;
+    }
     $.ajax({
       url: '/api/client/creation',
       data: JSON.stringify({mac: $('#macCreate').val(),name:$('#nameCreate').val()}),
@@ -14,7 +21,9 @@ $(document).ready(function(){
       contentType: "application/json; charset=utf-8"
     }).then(function(){
       $btn.text('添加主机');
-      $('.placeholder').text('添加成功').addClass('success');
+      alert("添加成功！");
+      //$('.placeholder').text('添加成功').addClass('success');
+      window.location.href = '/home';
     }, function() {
       $btn.text('添加主机');
       $('.placeholder').text('添加失败, 已存在的主机名或mac ').addClass('error');
