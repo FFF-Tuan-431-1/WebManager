c6e9c00c9c4503f6c07e7a36100a423545d12de4
lcjnil <emlcjnil@gmail.com>
2016-01-12 15:39:37 +0800

    fix bug

--- socket/index.js
+++ socket/index.js
@@ -21,40 +21,39 @@ module.exports = function(socket) {
       if (!client) {
         return Client.create({mac: data.mac, name: randomString(), isOnline:true});
       }
-      c = client;
       client.isOnline = true;
-      return client.save();
-    }).then(function() {
-      return c.getStates();
-    }).then(function(states) {
+      client.save();
+      return Promise.resolve(client);
+    }).then((client) => {
+      c = client;
+      return client.getStates();
+    }).then((states) => {
       if (states.length && states[states.length - 1].state === 'online') {
-        return Promise.resolve(null);
+        return;
       }
-      return c.createState({
+      c.createState({
         ip: data.ip,
         state: 'online'
-      })
+      });
     });
   });
 
   socket.on('disconnect',function(){
     console.log('offline client mac is ' + socket.data.mac);
-    var c = null;
 
     Client.findOne({where: {mac: socket.data.mac}}).then((client) => {
       client.isOnline = false;
-      c = client;
-      return client.save();
-    }) .then(function(){
-      return c.getStates();
-    }).then(function(states) {
-      if (states.length && states[states.length - 1].state === 'offline') {
-        return Promise.resolve(null);
-      }
-      return c.createState({
-        ip: socket.data.ip,
-        state: 'offline'
-      })
+      client.save().then(() => {
+        return client.getStates();
+      }).then((states) => {
+        if (states.length && states[states.length - 1].state === 'offline') {
+          return;
+        }
+        client.createState({
+          ip: socket.data.ip,
+          state: 'offline'
+        });
+      });
     });
   });
 };
