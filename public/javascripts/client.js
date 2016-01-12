f5045aef425188e3bbf4891855e3cc5f1d1aa23b
lcjnil <emlcjnil@gmail.com>
2016-01-12 14:13:52 +0800

    add labella

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
       };
     });
 
-    var formatDate = d3.time.format('%d %H:%m');
+    var formatDate = d3.time.format('%d日%H:%m');
 
     var options =   {
       margin: {left: 20, right: 20, top: 20, bottom: 20},
